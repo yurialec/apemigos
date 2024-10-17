d0adc81bb45bb0c09a7239aa495de8d78d558720
Yuri Alec <yuri.alec@hotmail.com>
2024-10-17 14:11:21 -0300

    melhorias site

--- resources/views/site.blade.php
+++ resources/views/site.blade.php
@@ -1,101 +1,87 @@
 @include('layouts.app')
+@include('partials.navbar')
 
 <div class="main-content container">
+    <div class="row justify-content-center align-items-center" style="min-height: 100vh;">
 
-    @include('partials.navbar')
-    @if (isset($mainText) && !empty($mainText))
-        <section id="hero" class="hero section dark-background">
-            <div class="container text-center" data-aos="fade-up" data-aos-delay="100">
-                <div class="row justify-content-center">
-                    <div class="col-lg-8">
-                        <h2>{{ $mainText->title }}</h2>
-                        <p>{{ $mainText->text }}</p>
-                    </div>
+        <!-- About Section -->
+        @if (isset($about) && !empty($about))
+            <section id="about" class="about section">
+                <div class="container section-title" data-aos="fade-up">
+                    <h2>Sobre Nós</h2>
                 </div>
-            </div>
-        </section>
-    @else
-    @endif
-
-    @if (isset($about) && !empty($about))
-        <section id="about" class="about section">
-            <div class="container section-title" data-aos="fade-up">
-                <h2>Sobre Nós</h2>
-            </div>
-            <div class="container">
-                <div class="row gy-3" style="background-color: #f8f9fa; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
-                    <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
-                        <img src="{{ 'storage/' . $about->image }}" alt="{{ $about->name }}" width="350" class="img-fluid">
-                    </div>
-
-                    <div class="col-lg-6 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
-                        <div class="about-content ps-0 ps-lg-3">
-                            <h3>{{ $about->title }}</h3>
-                            <p>{{ $about->description }}</p>
+                <div class="container">
+                    <div class="row gy-3" style="background-color: #f8f9fa; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+                        <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+                            <img src="{{ 'storage/' . $about->image }}" alt="{{ $about->name }}" width="350"
+                                class="img-fluid">
                         </div>
 
+                        <div class="col-lg-6 d-flex flex-column justify-content-center" data-aos="fade-up"
+                            data-aos-delay="200">
+                            <div class="about-content ps-0 ps-lg-3">
+                                <h3>{{ $about->title }}</h3>
+                                <p>{{ $about->description }}</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
-    @else
-    @endif
+            </section>
+        @endif
 
-    <!-- CAROUSEL -->
-    @if (isset($carousels) && $carousels->isNotEmpty())
-        <section class="empresa" style="background-color: #f8f9fa; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
-            <div class="container carousel-container">
-                <div class="d-flex justify-content-center my-5">
-                    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true"
-                        style="max-width: 800px;">
-                        <div class="carousel-indicators">
-                            @foreach ($carousels as $index => $carousel)
-                                <button style="background-color: #333; width: 14px; height: 14px; border-radius: 100%;"
-                                    type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="{{ $index }}"
-                                    class="{{ $index === 0 ? 'active' : '' }}"
-                                    aria-current="{{ $index === 0 ? 'true' : 'false' }}" aria-label="Slide {{ $index + 1 }}">
-                                </button>
-                            @endforeach
+        <!-- Carousel Section -->
+        @if (isset($carousels) && $carousels->isNotEmpty())
+            <section class="empresa" style="background-color: #f8f9fa; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+                <div class="container carousel-container">
+                    <div class="d-flex justify-content-center my-5">
+                        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true"
+                            style="max-width: 800px;">
+                            <div class="carousel-indicators">
+                                @foreach ($carousels as $index => $carousel)
+                                    <button style="background-color: #333; width: 14px; height: 14px; border-radius: 100%;"
+                                        type="button" data-bs-target="#carouselExampleIndicators"
+                                        data-bs-slide-to="{{ $index }}" class="{{ $index === 0 ? 'active' : '' }}"
+                                        aria-current="{{ $index === 0 ? 'true' : 'false' }}"
+                                        aria-label="Slide {{ $index + 1 }}">
+                                    </button>
+                                @endforeach
+                            </div>
+                            <div class="carousel-inner">
+                                @foreach ($carousels as $index => $carousel)
+                                    <div class="carousel-item {{ $index === 0 ? 'active' : '' }}">
+                                        <img src="{{ asset('storage/' . $carousel->image) }}" style="width: 550px;"
+                                            class="d-block h-40 mb-4" alt="...">
+                                        @if ($carousel->title || $carousel->description || $carousel->url_link || $carousel->name_link)
+                                            <div class="carousel-caption d-md-block"
+                                                style="background-color: #333; border-radius: 10px; opacity: 0.7;">
+                                                <h5 style="color: #fff;">{{ $carousel->title }}</h5>
+                                                <p>{{ $carousel->description }}</p>
+                                                <a style="color: #fff;" href="{{ $carousel->url_link }}"
+                                                    target="_blank">{{ $carousel->name_link }}</a>
+                                            </div>
+                                        @endif
+                                    </div>
+                                @endforeach
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+                                data-bs-slide="prev">
+                                <span style="background-color: #3498db; border-radius: 100%;"
+                                    class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+                                data-bs-slide="next">
+                                <span style="background-color: #3498db; border-radius: 100%;"
+                                    class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
                         </div>
-                        <div class="carousel-inner">
-                            @foreach ($carousels as $index => $carousel)
-                                <div class="carousel-item {{ $index === 0 ? 'active' : '' }}">
-                                    <img src="{{ asset('storage/' . $carousel->image) }}" style="width: 550px;"
-                                        class="d-block h-40 mb-4" alt="...">
-                                    @if ($carousel->title || $carousel->description || $carousel->url_link || $carousel->name_link)
-                                        <div class="carousel-caption d-md-block"
-                                            style="background-color: #333; border-radius: 10px; opacity : 0.7;">
-                                            <h5 style="color: #fff;">{{ $carousel->title }}</h5>
-                                            <p>{{ $carousel->description }}</p>
-                                            <a style="color: #fff;" href="{{ $carousel->url_link }}"
-                                                target="_blank">{{ $carousel->name_link }}</a>
-                                        </div>
-                                    @endif
-                                </div>
-                            @endforeach
-                        </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-                            data-bs-slide="prev">
-                            <span style="background-color: #3498db; border-radius: 100%;" class="carousel-control-prev-icon"
-                                aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-                            data-bs-slide="next">
-                            <span style="background-color: #3498db; border-radius: 100%;" class="carousel-control-next-icon"
-                                aria-hidden="true"></span>
-                            <span class="visually-hidden">Next</span>
-                        </button>
                     </div>
                 </div>
-            </div>
-        </section>
-    @else
-    @endif
-    <!-- END CAROUSEL -->
+            </section>
+        @endif
 
-    <!-- BLOG -->
-    <div class="container">
+        <!-- Blog Section -->
         @if (isset($siteblogs) && $siteblogs->isNotEmpty())
             <div class="container blog-section text-center">
                 <h2>Blog</h2>
@@ -120,49 +106,47 @@
                     @endforeach
                 </div>
             </div>
-        @else
         @endif
-    </div>
 
-
-    <footer class="footer light-background">
-        <div class="container footer-top">
-            <div class="row">
-                <div class="col-sm footer-about">
-                    <h4>Siga-nos</h4>
-                    <div class="social-links d-flex mt-4">
-                        @foreach ($socialmedias as $media)
-                            <a target="_blank" href="{{ $media->url }}">
-                                <i class="{{ $media->icon }}"></i>
-                            </a>
-                        @endforeach
+        <!-- Footer -->
+        <footer class="footer light-background">
+            <div class="container footer-top">
+                <div class="row">
+                    <div class="col-sm footer-about">
+                        <h4>Siga-nos</h4>
+                        <div class="social-links d-flex mt-4">
+                            @foreach ($socialmedias as $media)
+                                <a target="_blank" href="{{ $media->url }}">
+                                    <i class="{{ $media->icon }}"></i>
+                                </a>
+                            @endforeach
+                        </div>
                     </div>
-                </div>
 
-                <div class="col-sm footer-links">
-                    <ul>
-                        <li><a href="{{ route('index.site') }}">Início</a></li>
-                        <li><a href="{{ route('about') }}">Sobre</a></li>
-                        <li><a href="{{ route('contact') }}">Contato</a></li>
-                        <li><a href="{{ route('site.blog.index') }}">Blog</a>
-                        <li><a href="{{ route('login') }}">Área Restrita</a>
-                    </ul>
-                </div>
-
-                @if (isset($contact) && $contact)
-                    <div class="col-sm footer-contact">
-                        <h4>Contato</h4>
-                        <p>{{ $contact->address }}</p>
-                        <p>{{ $contact->city }}/{{ $contact->state }}</p>
-                        <p>{{ $contact->zipcode }}</p>
-                        <p class="mt-4"><strong>Telefone:</strong> <span>{{ $contact->phone }}</span></p>
-                        <p><strong>Email:</strong> <span>{{ $contact->email }}</span></p>
+                    <div class="col-sm footer-links">
+                        <ul>
+                            <li><a href="{{ route('index.site') }}">Início</a></li>
+                            <li><a href="{{ route('about') }}">Sobre</a></li>
+                            <li><a href="{{ route('contact') }}">Contato</a></li>
+                            <li><a href="{{ route('site.blog.index') }}">Blog</a></li>
+                            <li><a href="{{ route('login') }}">Área Restrita</a></li>
+                        </ul>
                     </div>
-                @else
-                @endif
+
+                    @if (isset($contact) && $contact)
+                        <div class="col-sm footer-contact">
+                            <h4>Contato</h4>
+                            <p>{{ $contact->address }}</p>
+                            <p>{{ $contact->city }}/{{ $contact->state }}</p>
+                            <p>{{ $contact->zipcode }}</p>
+                            <p class="mt-4"><strong>Telefone:</strong> <span>{{ $contact->phone }}</span></p>
+                            <p><strong>Email:</strong> <span>{{ $contact->email }}</span></p>
+                        </div>
+                    @endif
+                </div>
             </div>
-        </div>
-    </footer>
-
-    @include('partials.footer')
+        </footer>
+    </div>
 </div>
+
+@include('partials.footer')
